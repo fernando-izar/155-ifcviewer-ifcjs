8f19dfdc5e496a31467c961c99381947dc5bc414
fernando-izar <fizar@me.com>
2022-09-15 21:00:07 -0300

    need fix- doesn't work yet

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 import { Color } from "three";
 import { IfcViewerAPI } from "web-ifc-viewer";
 import { IFCLoader } from "web-ifc-three/IFCLoader";
+import {
+  IFCWALLSTANDARDCASE,
+  IFCSLAB,
+  IFCDOOR,
+  IFCWINDOW,
+  IFCFURNISHINGELEMENT,
+  IFCMEMBER,
+  IFCPLATE,
+} from "web-ifc";
 
 const container = document.getElementById("viewer-container");
 const viewer = new IfcViewerAPI({
@@ -22,6 +31,8 @@ input.addEventListener("change", async (changed) => {
     const model = await viewer.IFC.loadIfcUrl(url);
     await viewer.shadowDropper.renderShadow(model.modelID);
     // viewer.context.renderer.postProduction.active = true;
+
+    await setupAllCategories();
 
     const ifcProject = await viewer.IFC.getSpatialStructure(model.modelID);
     console.log(ifcProject);
@@ -126,4 +137,70 @@ function removeAllChildren(element) {
   while (element.firstChild) {
     element.removeChild(element.firstChild);
   }
+}
+
+const scene = viewer.context.getScene();
+console.log("scene", scene);
+
+// List of categories names
+const categories = {
+  IFCWALLSTANDARDCASE,
+  IFCSLAB,
+  IFCFURNISHINGELEMENT,
+  IFCDOOR,
+  IFCWINDOW,
+  IFCPLATE,
+  IFCMEMBER,
+};
+
+// Gets the name of a category
+function getName(category) {
+  const names = Object.keys(categories);
+  return names.find((name) => categories[name] === category);
+}
+
+// Gets the IDs of all the items of a specific category
+async function getAll(category) {
+  return viewer.IFC.loader.ifcManager.getAllItemsOfType(0, category, false);
+}
+
+// Creates a new subset containing all elements of a category
+async function newSubsetOfType(category) {
+  const ids = await getAll(category);
+  return viewer.IFC.loader.ifcManager.createSubset({
+    modelID: 0,
+    scene,
+    ids,
+    removePrevious: true,
+    customID: category.toString(),
+  });
+}
+
+// Stores the created subsets
+const subsets = {};
+
+async function setupAllCategories() {
+  const allCategories = Object.values(categories);
+  for (let i = 0; i < allCategories.length; i++) {
+    const category = allCategories[i];
+    await setupCategory(category);
+  }
+}
+
+// Creates a new subset and configures the checkbox
+async function setupCategory(category) {
+  subsets[category] = await newSubsetOfType(category);
+  setupCheckBox(category);
+}
+
+// Sets up the checkbox event to hide / show elements
+function setupCheckBox(category) {
+  const name = getName(category);
+  const checkBox = document.getElementById(name);
+  checkBox.addEventListener("change", (event) => {
+    const checked = event.target.checked;
+    const subset = subsets[category];
+    if (checked) scene.add(subset);
+    else subset.removeFromParent();
+  });
 }
